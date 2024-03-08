89619105cdf5b73f8f02ac5410b89a32e4fd3408
isimrangupta <simrandgupta95@gmail.com>
2024-03-08 22:18:34 +0530

    learning JS

---
+++ operators.js
@@ -0,0 +1,83 @@
+// console.log(oprators of Js)
+
+// Arithmetic operators.....
+let a = 6;
+let b = 4;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a ** b);
+console.log(a / b);
+console.log(a % b);
+// console.log(++a);
+// console.log(a++);
+// console.log(a);
+console.log(a--);
+console.log(a);
+
+let c = 8;
+console.log(++c);
+console.log(c++);
+console.log(--c);
+console.log(c--);
+console.log(c);
+console.log(c--);
+
+
+// Assignment Opefators.....
+z = 20
+console.log(z);
+
+z += 5    // same as z = z + 5;
+console.log(z);
+
+z -= 5    // same as z = z - 5;
+console.log(z);
+
+z /= 5    // same as z = z / 5;
+console.log(z);
+
+z *= 5    // same as z = z * 5;
+console.log(z);
+
+z **= 5    // same as z = z ** 5;
+console.log(z);
+
+z %= 5    // same as z = z % 5;
+console.log(z);
+
+
+
+// Comparison operators....
+let com1 = 6;
+let com2 = 7;
+console.log(com1 == com2);
+console.log(com1 != com2);
+
+let x = 3;
+let y = "3";
+console.log(x === y);
+console.log(x !== y);
+
+let r = 6;
+let s = 5;
+console.log(r > s);
+console.log(r >= s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,54 @@
+console.log("Hey this is tutorial 1")
+
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "Simran";
+let _a = "Vishal";
+// var 55a = "Diksha";   // NOT ALLOWED
+
+
+// console.log(a + b + 8)
+
+// console.log(typeof a, typeof b, typeof c,)
+
+{
+    var a = 66;
+    console.log(a) 
+}
+
+    console.log(a)
+
+
+// const a1 = 6;
+// a1 = a1 + 1      // NOT ALLOWED becouse a1 is constant
+
+let x = "Hurry bhai"
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+// let o = {
+//     name:"Simran",
+//     "job code": 5600, 
+//     "is_handsome": true
+// }
+// console.log(o);
+
+// o.salary = "100crores"
+// console.log(o);
+// o.salary = "500crores"
+// console.log(o);
+
+let o ={
+    name: "Diksha institute ",
+    "phone number": 1234,
+    "mark of a student": 500,
+}
+
+console.log(o);
