354a60323e4e1c08f4d7059f2298c3795af02320
isimrangupta <simrandgupta95@gmail.com>
2024-03-10 19:50:44 +0530

    conditional.js add

---
+++ conditional.js
@@ -0,0 +1,54 @@
+// If statement....
+/* let age = 1;
+
+if (age < 18){
+    console.log("you can not drive")
+} */
+
+
+
+// If-else statement....
+/* const age = 18;
+
+if (age >= 18) {
+    console.log("you can vote");
+}
+else {
+    console.log("you cannt vote");
+} */
+
+
+// If...else if...else statement......
+/* const marks = 76;
+
+if(marks > 90){
+    console.log("A+");
+}
+else if(marks > 80){
+    console.log("A");
+}
+else if(marks > 70){
+    console.log("B+");
+}
+else if(marks > 60){
+    console.log("B");
+}
+else {
+    console.log("Low marks");
+} */
+
+// Ternary operator....
+const marks = 50;
+
+const result = (marks >= 40) ? "passed" : "failed";
+console.log("result", result);
+ 
+
+
+
+
+
+
+
+
+
