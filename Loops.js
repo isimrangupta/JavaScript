fc628c6dc9f13d947326911fa7c84d7c69bba60d
isimrangupta <simrandgupta95@gmail.com>
2024-03-13 21:06:17 +0530

    Loops.JS

---
+++ Loops.js
@@ -0,0 +1,61 @@
+// for loop
+/*
+for (let i = 0; i < 100; i++) {
+   console.log(i + 1);
+    
+}
+*/
+
+
+
+// For in loop
+/*
+let obj = {
+    name: "Simran",
+    role: "Programmer", 
+    company: "CodewithSimran AI"
+}
+
+for (const key in obj) {
+        const element = obj[key];
+        console.log(key, element)
+        
+}
+/*
+
+
+
+
+
+// For of loop
+/*
+for (const s of "Sangita") {
+    console.log(s)
+}
+*/
+
+
+
+
+// While loop
+/* 
+ let i = 0;
+while (i < 6) {
+    console.log(i)
+    i++;
+}
+*/
+
+
+
+
+// Do While loop
+/*
+let i = 6;
+do {
+    console.log(i)
+    i++;
+} while (i < 6);
+*/
+
+
