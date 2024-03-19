755ff1fbb77b063becdc35dfe5856410fbf80235
isimrangupta <simrandgupta95@gmail.com>
2024-03-19 22:15:58 +0530

    Strings added

---
+++ Strings.js
@@ -0,0 +1,35 @@
+// This is Strings tutorial
+let a = "Simran";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+// console.log(a[6]);
+
+console.log(a.length);
+
+
+
+let fullname = "Diksha";
+let friend = "Vishal"
+console.log("His name is " + fullname + " and his friend name is " + friend);
+//Template Literals
+console.log(`His name is ${fullname} and his friends name is ${friend}`);
+
+
+b = "Vishal";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1, 4));
+console.log(b.slice(1));
+
+console.log(b.replace("Vi","82"));
+
+console.log(b.concat(a));
+console.log(b.concat(a, "Diksha"));
+
+
