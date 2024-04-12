f1a50df877664acdf674f8d2484a98c43b9fcfa4
isimrangupta <simrandgupta95@gmail.com>
2024-04-12 20:55:20 +0530

    Scope level.js added

---
+++ Scope level.js
@@ -0,0 +1,52 @@
+
+function one () {
+    const userName = "Simran"
+
+    function two() {
+        const website = "youtube"
+        console.log(userName);    
+    }
+    // console.log(website);      
+
+    two()
+}
+
+one()
+
+
+
+
+if (true) {
+    const username = "Diksha"
+    if (username === "Diksha") {
+        const title = " Gupta"
+        console.log(username + title);
+    }
+    // console.log(title);    
+}
+
+// console.log(username);    
+
+
+
+// ++++++++++++++++++++++ intersting +++++++++++++++++++++++++++++++++++
+
+
+console.log(addone(5));  
+function addone(num) {
+    return num + 1
+}
+
+// console.log(addone(5));     
+
+
+
+//_____________________________________________________________
+
+// console.log(addTwo(5));           
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(5));           
+
