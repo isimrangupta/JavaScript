72ba76a325f0af6c12782e3a6d355918c54f407b
isimrangupta <simrandgupta95@gmail.com>
2024-04-20 12:23:17 +0530

    For loop with break and continue added

---
+++ For loop with break and continue.js
@@ -0,0 +1,61 @@
+// for
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if (element === 5) {
+        console.log("5 is best number");
+    }
+    console.log(element);                 
+}
+
+
+// you can write the loop inside the loop :-
+for (let i = 0; i < 10; i++) {
+    console.log(`outer loop value: ${i}`);
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner loop value ${j} and inner loop value ${i}`);
+    }
+}
+
+
+// well inside this loop, you could have done other things like this Multiplication etc.
+
+for (let i = 0; i < 10; i++) {
+    console.log(`outer loop value: ${i}`);
+    for (let j = 0; j < 10; j++) {
+       console.log(i + '*' + j + ' = ' + i*j);
+    }
+}
+
+
+// Array in loop :-
+myArray = ["Superman", "Spiderman", "Saktimaan", "Batman"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
+
+
+// 1) Break 
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        break
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
+
+
+// 2) Continue 
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
